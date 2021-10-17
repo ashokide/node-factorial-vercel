d7138668d58502376235a951fd0aa9d94ea83fee
Ashok N <66304695+ashokide@users.noreply.github.com>
2021-10-17 22:46:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.get('/factorial/:n',(req,res)=>{
     let n = parseInt(req.params.n);
     let fact = 1;
